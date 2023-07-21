2045dffbe64b582b1d29c261589e01b0f5092578
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-07-21 14:35:12 +0700

    Dashboard Baru

--- view/dashboard.php
+++ view/dashboard.php
@@ -7,13 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</head>
+  </head>
 <body>
     <div class="container-fluid">
         <div class="container">
             <div class="row">
                 <div class="col-md-12 pt-5">
-                    <h3> Welcome To SMT Banner</h3>
+                    <h3 style="color:#F87474"> Welcome To SMT Banner</h3>
                 </div>
                 <div class="card col-md-11 ms-2 mt-3">
                    <h4> Description : </h4>
@@ -22,7 +22,7 @@
                 <div class="col-md-3 mt-3">
                     <div class="search">
                         <i class="fa fa-search"></i>
-                        <input type="text" class="form-control" placeholder="Search">
+                        <input type="text" class="form-control" placeholder="Search...">
                         <button><box-icon name='search' color='#000' class="search_icon" ></box-icon></button>
                     </div>
                 </div>
@@ -31,7 +31,7 @@
                 </div>
                 <div class="col-md-11 pt-4">
                     <table class="table table-bordered table-hover">
-                        <thead class="thead-dark">
+                    <thead class="table-light">
                             <tr>
                                 <th scope="col"> No </th>
                                 <th scope="col"> Title </th>
@@ -46,10 +46,10 @@
                                 <td> Projek 1 </td>
                                 <td> Parallax </td>
                                 <td> Parallax "1"</td>
-                                <td> 
-                                    <box-icon type='solid' name='copy'></box-icon> 
-                                    <box-icon type='solid' name='message-square-edit'></box-icon>
-                                    <box-icon type='solid' name='message-square-minus'></box-icon>
+                                <td class="text-center"> 
+                                    <box-icon type='solid' name='copy' color="#3AB0FF"></box-icon> 
+                                    <box-icon type='solid' name='message-square-edit' color="#FFB562"></box-icon>
+                                    <box-icon type='solid' name='message-square-minus' color="#F87474"></box-icon>
                                 </td>
                             </tr>
                         </tbody>
@@ -70,38 +70,37 @@
       </div>
       <div class="modal-body">
      <form action="" method="post">
-                  <div class="form-group">
-                    <label for="recipient-name" class="col-form-label">Title:&nbsp;</label>
-                    <input type="text" class="form-control" id="recipient-name" name="title" required placeholder="Title Slider"  autocomplete="off">
-                  </div>
-                 <div class="select d-flex">
-                 <label for="recipient-name" class="col-form-label">Title:&nbsp;</label>
-                    <select name="class" >
-                        <option value="">Choose Type</option>
-                        <?php
-                        global $wpdb;
-                        $table_name = $wpdb->prefix . 'smt_type'; 
-                        $query = "SELECT * FROM $table_name";
-                        $results = $wpdb->get_results($query);
-                        if ($results) {
-                        ob_start();  
-                        ?>
-                         <?php foreach ($results as $result) { ?>
-                        <option value="<?php echo $result->type; ?>">-- <?php echo $result->type; ?> -- </option> 
-                        <?php
-                        }
-                        ?>
-                    </select>
-                    </div>
-                    <?php
-                                    $output = ob_get_clean();
-                                    echo $output;
-                                    } else {
-                                    echo 'Tidak ada data,Coba mulailah untuk menambahkan!!';
-                                    }
-                                    ?>
-                                    
-      </div>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Title:&nbsp;</label>
+            <input type="text" class="form-control" id="recipient-name" name="title" required placeholder="Title Slider"  autocomplete="off">
+          </div>
+          <div class="select d-flex">
+          <label for="recipient-name" class="col-form-label">Title:&nbsp;</label>
+            <select name="class" >
+                <option value="">Choose Type</option>
+                <?php
+                global $wpdb;
+                $table_name = $wpdb->prefix . 'smt_type'; 
+                $query = "SELECT * FROM $table_name";
+                $results = $wpdb->get_results($query);
+                if ($results) {
+                ob_start();  
+                ?>
+                  <?php foreach ($results as $result) { ?>
+                <option value="<?php echo $result->type; ?>">-- <?php echo $result->type; ?> -- </option> 
+                <?php
+                }
+                ?>
+            </select>
+            </div>
+            <?php
+            $output = ob_get_clean();
+            echo $output;
+            } else {
+            echo 'Tidak ada data,Coba mulailah untuk menambahkan!!';
+            }
+            ?>                    
+        </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
         <button type="submit" class="btn btn-primary" id="simpan" name="simpan">Tambah</button>
@@ -114,30 +113,38 @@
 </html>
 
 <style>
-.body{
+body{
 background-color:#F9F2ED;    
+font-family:FUTURA MD BT;
 }
 
 @import url("https://fonts.googleapis.com/css2?family=Poppins:weight@100;200;300;400;500;600;700;800&display=swap");
 .search{
 position: relative;
 box-shadow: 0 0 40px rgba(51, 51, 51, .1);
+
 }
 
 .search input{
  height: 40px;
- border: 2px solid #d6d4d4;
+ border : none;
+ border-radius:15px;
+ background-color:#FFB562;
 }
 
 .search input:focus{
  box-shadow: none;
- border: 2px solid blue;
-
+ border : none;
+ border-radius:15px;
+ background-color:#FFB562;
 }
 
 .search_icon{
-    width:15px;
-    height: 15px;
+    margin-top:5px;
+    width:18px;
+    height: 18px;
+    background-color:#FFB562;
+    color: #F87474;
 }
 
 .search button{
@@ -148,6 +155,7 @@ box-shadow: 0 0 40px rgba(51, 51, 51, .1);
  width: 30px;
  background: white;
  border:none;
+ background-color:#FFB562;
 }
 
 /* CSS */
@@ -172,7 +180,7 @@ box-shadow: 0 0 40px rgba(51, 51, 51, .1);
 .button-32:not(:disabled):hover,
 .button-32:not(:disabled):focus {
   outline: 0;
-  background: #f4e603;
+  background: #FFB562;
   box-shadow: 0 0 0 2px rgba(0,0,0,.2), 0 3px 8px 0 rgba(0,0,0,.15);
 }
 
