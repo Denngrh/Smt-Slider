db612e704876533966bd4b550c8a3291b6c2448e
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-07-25 09:31:47 +0700

    view setting

--- hook.php
+++ hook.php
@@ -2,7 +2,7 @@
 function ListMenu(){
     add_menu_page ('Plugin Slider', 'Smt Slider','','list', 'list_menu','dashicons-email');
     add_submenu_page('list','List_sub_menu','Dashboard','manage_options','dashboard','list_menu');
-    add_submenu_page('list','List_sub_menu','Setting','manage_options','setting','list_menu_2');
+    add_submenu_page('','List_sub_menu','Setting','manage_options','setting','list_menu_2');
 }
 
 function list_menu() {
@@ -12,7 +12,9 @@ function list_menu_2(){
     include_once 'view/setting.php';
 }
 
-// ADD MENU
+
+
+
 add_action('admin_menu','ListMenu');
 ?>
 
--- view/setting.php
+++ view/setting.php
@@ -8,7 +8,7 @@
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="sticky-menu">
     TES
 </body>
 </html>
