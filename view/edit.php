0ccfc18f5c76c78a963024d458b793f8cd57dc6b
MDafaAzharya <125567760+MDafaAzharya@users.noreply.github.com>
2023-07-26 14:14:48 +0700

    Edit Page with image

--- view/edit.php
+++ view/edit.php
@@ -48,11 +48,72 @@
                         <span>px</span>
                     </div>
                 </div>
-                    <div class="col-md-2 offset-md-9 mt-3">
-                        <button class="button-18" role="button">Add</button>
+                <?php
+                if ( isset( $_POST['submit_image_selector'] ) && isset( $_POST['image_attachment_id'] ) ) :
+                        update_option( 'media_selector_attachment_id', absint( $_POST['image_attachment_id'] ) );
+                    endif;
+                    wp_enqueue_media();
+                    ?><div class="mt-3 ">
+                        <form method='post'>
+                        
+                        <input id="upload_image_button" type="button" class="button" value="<?php _e( 'Upload image' ); ?>" />
+                        <input type='hidden' name='image_attachment_id' id='image_attachment_id' value='<?php echo get_option( 'media_selector_attachment_id' ); ?>'>
+                        <input type="submit" name="submit_image_selector" value="Save" class="button-primary">
+                    </form>
                     </div>
+                <?php
+                $my_saved_attachment_post_id = get_option( 'media_selector_attachment_id', 0 );
+                ?><script type='text/javascript'>
+                    jQuery( document ).ready( function( $ ) {
+                        // Uploading files
+                        var file_frame;
+                        var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
+                        var set_to_post_id = <?php echo $my_saved_attachment_post_id; ?>; // Set this
+                        jQuery('#upload_image_button').on('click', function( event ){
+                            event.preventDefault();
+                            // If the media frame already exists, reopen it.
+                            if ( file_frame ) {
+                                // Set the post ID to what we want
+                                file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
+                                // Open frame
+                                file_frame.open();
+                                return;
+                            } else {
+                                // Set the wp.media post id so the uploader grabs the ID we want when initialised
+                                wp.media.model.settings.post.id = set_to_post_id;
+                            }
+                            // Create the media frame.
+                            file_frame = wp.media.frames.file_frame = wp.media({
+                                title: 'Select a image to upload',
+                                button: {
+                                    text: 'Use this image',
+                                },
+                                multiple: false // Set to true to allow multiple files to be selected
+                            });
+                            // When an image is selected, run a callback.
+                            file_frame.on( 'select', function() {
+                                // We set multiple to false so only get one image from the uploader
+                                attachment = file_frame.state().get('selection').first().toJSON();
+                                // Do something with attachment.id and/or attachment.url here
+                                $( '#image-preview' ).attr( 'src', attachment.url ).css( 'width', 'auto' );
+                                $( '#image_attachment_id' ).val( attachment.id );
+                                // Restore the main post ID
+                                wp.media.model.settings.post.id = wp_media_post_id;
+                            });
+                                // Finally, open the modal
+                                file_frame.open();
+                        });
+                        // Restore the main ID when the add media button is pressed
+                        jQuery( 'a.add_media' ).on( 'click', function() {
+                            wp.media.model.settings.post.id = wp_media_post_id;
+                        });
+                    });
+            </script>
                 <!-- Form Image -->
                 <div class="col border my-3">
+                        <div class='image-preview-wrapper mt-3  offset-md-1'>
+                            <img id='image-preview' style="border: 2px solid black;" src='<?php echo wp_get_attachment_url( get_option( 'media_selector_attachment_id' ) ); ?>' width='200'>
+                        </div>
                         <div class="my-3 d-flex justify-content-center">
                             <img src="images.jpeg" alt="" srcset="" width="80%">
                         </div>
